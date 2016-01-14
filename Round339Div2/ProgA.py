ac39942db8ae2ea9cfee24a2c64737a929e1de49
yumechi <yumechi@users.noreply.github.com>
2016-01-15 05:17:13 +0900

    Round 339 ProgA

---
+++ Round339Div2/ProgA.py
@@ -0,0 +1,12 @@
+l, r, k = map(int, input().split())
+res = []
+if k > r:
+    print("-1" if l != 1 else "1")
+    exit(0)
+num = 1
+while num < l:
+    num *= k
+while l <= num <= r:
+    res.append(str(num))
+    num *= k
+print("-1" if len(res) == 0 else " ".join(res))
